7b3f0e55bb10a4239c6005cd559634da9fe6423c
Synnex-DevOps <84031494+synnex-devops@users.noreply.github.com>
2021-09-03 11:59:50 -0300

    Add files via upload

---
+++ app_config.py
@@ -0,0 +1,28 @@
+import os
+
+CLIENT_ID = "57157df1-24ad-41b7-ae45-c649e9b947ab" # Application (client) ID of app registration
+
+CLIENT_SECRET = "Q~53l3Kkf5Q~-tn_87w-je1HMzzXnbf579" # Placeholder - for use ONLY during testing.
+# In a production app, we recommend you use a more secure method of storing your secret,
+# like Azure Key Vault. Or, use an environment variable as described in Flask's documentation:
+# https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
+# CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+# if not CLIENT_SECRET:
+#     raise ValueError("Need to define CLIENT_SECRET environment variable")
+
+AUTHORITY = "https://login.microsoftonline.com/a4765b03-ef37-4988-ba30-2a7dc3e220be"  # For multi-tenant app
+# AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
+
+REDIRECT_PATH = "/getAToken"  # Used for forming an absolute URL to your redirect URI.
+                              # The absolute URL must match the redirect URI you set
+                              # in the app's registration in the Azure portal.
+
+# You can find more Microsoft Graph API endpoints from Graph Explorer
+# https://developer.microsoft.com/en-us/graph/graph-explorer
+ENDPOINT = 'https://graph.microsoft.com/v1.0/users'  # This resource requires no admin consent
+
+# You can find the proper permission names from this document
+# https://docs.microsoft.com/en-us/graph/permissions-reference
+SCOPE = ["User.ReadBasic.All"]
+
+SESSION_TYPE = "filesystem"  # Specifies the token cache should be stored in server-side session
